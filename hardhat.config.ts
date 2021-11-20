7ce38c831ef7ee1f6ff555ba8b35c2542ce00071
ozayed <omar.zayed@tum.de>
2021-11-20 11:46:42 +0100

    complete test suite

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.0",
+};
